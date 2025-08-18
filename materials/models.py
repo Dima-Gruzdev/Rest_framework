5cf85f87de030d73d5f88e64f1f50b794529b3b5
Дмитрий Груздев <nubile4446@mail.ru>
2025-08-18 16:47:53 +0300

    Django-Framework project home

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание курса", help_text="Опишите курс"
+    )
+    preview = models.ImageField(
+        upload_to="materials/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение",
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="courses",
+        verbose_name="Владелец курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Укажите название урока",
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание урока", help_text="Опишите урок"
+    )
+    preview = models.ImageField(
+        upload_to="materials/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение",
+    )
+    video_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на видео",
+        help_text="Введите cсылку на видео",
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс"
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="Владелец урока",
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return f"{self.name} {self.description} {self.video_url}"
